55922d18fb4c83dab33f300b9bac8be9c59e13cf
aangelovv <112356522+aangelovv@users.noreply.github.com>
2023-08-05 02:37:34 +0300

    Add Parse/Back4App database

--- src/App.js
+++ src/App.js
@@ -9,11 +9,17 @@ import OtherVehicles from "./components/OtherVehicles/OtherVehicles";
 
 import "./App.css";
 
+import Parse from "parse/dist/parse.min.js";
 
 function NotFound() {
   return <h1>404 Not Found</h1>;
 }
 
+const PARSE_APPLICATION_ID = "zGhlCvGoaMHN2tWoQt3fIN4Ta1tYPaaZObwaAAqR";
+const PARSE_HOST_URL = "https://parseapi.back4app.com/";
+const PARSE_JAVASCRIPT_KEY = "4JOaaym3W4WxVdIiZbcw0tIBwucnC5kcM2lgAbHo";
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 const App = () => {
   return (
