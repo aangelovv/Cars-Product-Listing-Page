5b792a45ff0c8cc0839539de064ff5147e518c30
aangelovv <112356522+aangelovv@users.noreply.github.com>
2023-08-05 02:36:33 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "./components/Header/Header";
+import Cars from "./components/Cars/Cars";
+import Vans from "./components/Vans/Vans";
+import Motorcycles from "./components/Motorcycles/Motorcycles";
+import OtherVehicles from "./components/OtherVehicles/OtherVehicles";
+
+import "./App.css";
+
+
+function NotFound() {
+  return <h1>404 Not Found</h1>;
 }
+
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Cars />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/motorcycles" element={<Motorcycles />} />
+        <Route path="/other-vehicles" element={<OtherVehicles />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
