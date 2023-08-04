6bb6f7a56e3b6c249c90e966cfebbf62ce3cbed9
aangelovv <112356522+aangelovv@users.noreply.github.com>
2023-08-04 18:33:35 +0300

    Add carCard component and styling

---
+++ src/components/Cars/CarsCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import styles from "./CarsCard.module.css";
+
+const showAlertHandler = () => {
+  alert("Vehicle has been added to saved!");
+};
+
+const CarsCard = (props) => {
+  return (
+    <article className={styles.cars}>
+      <article className={styles["car-img-wrapper"]}>
+        <img src={props.cars.picture} alt="car fleet img" />
+      </article>
+
+      <p className={styles["car-name"]}>{props.cars.name}</p>
+      <p className={styles["vehicle-description"]}>
+        Model: {props.cars.description}
+      </p>
+      <p className={styles["vehicle-description"]}>
+        Pirce: {props.cars.price} $
+      </p>
+      <p className={styles["vehicle-description"]}>Color: {props.cars.color}</p>
+      <button onClick={showAlertHandler} className={styles["btn"]}>
+        Add to saved
+      </button>
+    </article>
+  );
+};
+
+export default CarsCard;
