b702a617762fb1023a040c472bc7a72d88969ff2
aangelovv <112356522+aangelovv@users.noreply.github.com>
2023-08-05 20:31:03 +0300

    Add responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import styles from "./Header.module.css";
 import logo from "../../assets/car-logo.png";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
@@ -11,28 +14,55 @@ const Header = () => {
             <img src={logo} alt="Car Company logo" className={styles.img} />
           </Link>
         </li>
-        <div className={styles["links-wrapper"]}>
+        <div
+          className={
+            isOpen ? styles["links-wrapper-open"] : styles["links-wrapper"]
+          }
+        >
           <li className={styles.links}>
-            <Link to="/cars" className={styles.link}>
+            <Link
+              to="/cars"
+              className={styles.link}
+              onClick={() => setIsOpen(false)}
+            >
               Cars
+              <i className={`${styles.icon} fas fa-car`} />
             </Link>
           </li>
           <li className={styles.links}>
-            <Link to="/vans" className={styles.link}>
+            <Link
+              to="/vans"
+              className={styles.link}
+              onClick={() => setIsOpen(false)}
+            >
               Vans
+              <i className={`${styles.icon} fas fa-shuttle-van`} />
             </Link>
           </li>
           <li className={styles.links}>
-            <Link to="/motorcycles" className={styles.link}>
+            <Link
+              to="/motorcycles"
+              className={styles.link}
+              onClick={() => setIsOpen(false)}
+            >
               Motorcycles
+              <i className={`${styles.icon} fas fa-motorcycle`} />
             </Link>
           </li>
           <li className={styles.links}>
-            <Link to="/other-vehicles" className={styles.link}>
+            <Link
+              to="/other-vehicles"
+              className={styles.link}
+              onClick={() => setIsOpen(false)}
+            >
               Other Vehicles
+              <i className={`${styles.icon} fas fa-tractor`} />
             </Link>
           </li>
         </div>
+        <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+          <i className="fas fa-bars"></i>
+        </button>
       </nav>
     </header>
   );
