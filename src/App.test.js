64819949362dd4e456ccb60d3e3cb897f657b97b
aangelovv <112356522+aangelovv@users.noreply.github.com>
2023-10-19 18:55:19 +0300

    Changed test

--- src/App.test.js
+++ src/App.test.js
@@ -31,6 +31,6 @@ test("renders Motorcycles link", () => {
 
 test("renders Other vehicles link", () => {
   render(<OtherVehicles />);
-  const linkElement = screen.getByText(/Other vehicles/i);
+  const linkElement = screen.getByText(/Other Vehicles/i);
   expect(linkElement).toBeInTheDocument();
 });
